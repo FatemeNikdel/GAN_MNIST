a7695d373f84b2cccd74b5f64bb93f54ca36fd71
FatemeNikdel <nikdel_fateme@yahoo.com>
2022-11-26 12:55:48 +0330

    GAN is DONE!!!!!!!!!!

--- GAN.py
+++ GAN.py
@@ -3,15 +3,12 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import numpy as np
-import matplotlib.pyplot as plt
-
-##########################################   Variables    ############################################################
-
-Batch_size = 32
 
 
-
+############################################  Variable     ###########################################################
+Batch_size = 256
+noise_dim = 100
+epochs = 5
 ############################################  Generator    ###########################################################
 
 def generator(data):
@@ -45,7 +42,7 @@ def descriminator():
                                        ])
     opt = tf.keras.optimizers.Adam(learning_rate = 0.002, beta_1 = 0.5)
     d_net.compile(optimizer = opt, loss="binary_crossentropy", metrics = ['accuracy'])
-    d_net.summary()
+    #d_net.summary()
     return d_net
 
 d_model = descriminator()
@@ -59,7 +56,7 @@ def Gan( g_model , d_model):
         d_model
     ])
 
-    opt = tf.keras.optimizers.Adam(leraning_rate = 0.0002, beta_1 = 0.5)
+    opt = tf.keras.optimizers.Adam(learning_rate = 0.0002, beta_1 = 0.5)
     gan_net.compile(loss = "binary_crossentropy", metrics = ['accuracy'])
     return gan_net
 
@@ -75,7 +72,7 @@ def Noise_data(noise_dim, batch_size):
 def fake_data(g_model, noise_dim, batch_size):
     x_input_Generator = Noise_data(noise_dim, batch_size)
     X = g_model.predict(x_input_Generator)
-    y = np.zeros((noise_dim,1))
+    y = np.zeros((batch_size,1))
     return X, y
 
 x_fake , y_fake = fake_data(g_model, 100, 32)
@@ -89,15 +86,15 @@ def Real_data():
 real_data = Real_data()
 
 def real_sample(real_data, batch_size):
-    index = np.random.randint(0, len(real_data), batch_size)  # generate random index in size of batch data between [o , len real data]
-    x = real_data(index)                                      # find the data of indexes
+    index = np.random.randint(0, real_data.shape[0], batch_size)  # generate random index in size of batch data between [o , len real data]
+    x = real_data[index]                                      # find the data of indexes
     y = np.ones((batch_size,1))                               # assign the label of real data which is 1
 
     return x , y
 
 x_real , y_real = real_sample(real_data, Batch_size)
 
-##############################################    showw result     ####################################################
+##############################################    show result     ####################################################
 
 def save_plot(examples, epoch, n=10):
 
@@ -117,7 +114,7 @@ def save_plot(examples, epoch, n=10):
 
 def summarize_performance(epoch, g_model, d_model, dataset, noise_dim, batch_size=100):
 
-    X_real, y_real = Real_data(dataset, batch_size)
+    X_real, y_real = real_sample(dataset, batch_size)
 
     _, acc_real = d_model.evaluate(X_real, y_real, verbose=0)
 
@@ -141,16 +138,16 @@ def train(d_model, g_model, gan_model, real_data, noise_dim, epochs, batch_size)
     for i in range(epochs):
         for j in range(batch_per_epoch):
             # train descriminator
-            x_real, y_real = real_sample(real_data, batch_size)
-            x_fake , y_fake = fake_data(g_model, noise_dim, batch_size)
+            x_real, y_real = real_sample(real_data, half_batch)
+            x_fake , y_fake = fake_data(g_model, noise_dim, half_batch)
             X , y = np.vstack((x_real, x_fake)), np.vstack((y_real, y_fake))
             d_loss , _ = d_model.train_on_batch(X, y)
             # train gan
             x_gan = Noise_data(noise_dim, batch_size)
             y_gan = np.ones((batch_size , 1))
-            g_loss, _ = Gan.train_on_batch(x_gan, y_gan)
+            g_loss, _ = gan_model.train_on_batch(x_gan, y_gan)
             # show results
             print( f"===>>>>{i+1},{j+1}/{batch_per_epoch}, d = {d_loss:.3f}, g = {g_loss:.3f}")
             if (i+1) % 10 == 0:
                 summarize_performance(i, g_model, d_model, real_data, noise_dim)
-
+train(d_model, g_model, gan_model, real_data, noise_dim, epochs, Batch_size)
